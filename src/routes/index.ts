f23d589422ee47e1b9412f29e9e5de5e01c08919
guilherme <guilherme_pbartoli@yahoo.com>
2024-04-04 21:02:02 -0300

    Atividade 4

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import taskRoutes from './task/task.routes'
 import authRoutes from './auth/auth.routes'
-import notesRoutes from './dependant/dependant.routes'
+import notesRoutes from './notes/notes.routes'
 
 const routes = Router()
 
